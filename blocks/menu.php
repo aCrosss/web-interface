d95525bdbd815c7a04161904456884bc48ed8eb3
M1lya <gilyazova2001@bk.ru>
2021-07-26 16:59:13 +0300

    Create menu.php

---
+++ blocks/menu.php
@@ -0,0 +1,32 @@
+<div id="menu">   
+                <ul>
+                    <li><a href=main.php>Главная</a></li>
+                    <li><details>
+                      <summary>Базовая настройка</summary>
+                        <ul>
+                            <li><a href="net-interface.php">Сетевой интерфейс</a></li>
+                            <li><a href="serial port.php"> Последовательный порт</a></li>
+                            <li><a href="discrete.php">Дискретные I/O и реле</a><li>
+                            <li><a href="routes.php">Маршруты</a></li>
+                            <li><a href="logging.php"> Журналирование</a></li>
+                            <li><a href="managing.php"> Управление сервисами</a></li>
+                        </ul>
+                    </details>
+                    </li>
+                    <li>
+                      <details>
+                      <summary>Сервис</summary>
+                        <ul>
+                            <li><a href="log.php"> Журнал</a></li>
+                            <li><a href="firmware update.php"> Обновление прошивки</a></li>
+                            <li><a href="import.php"> Импорт конфигурации</a></li>
+                            <li><a href="export.php"> Экспорт конфигурации</a></li>
+                            <li><a href="factory configuration.php"> Заводская конфигурация</a></li>
+                            <li><a href="password.php"> Изменение пароля</a></li>
+                          </ul>
+                      </details>
+                    </li>
+                    <li><a href="saving settings.php"> Сохранить настройки</a></li>
+                    <li><a href="restart.php"> Перезапуск</a></li>
+                    <li><a href="information.php"> Информация</a></li>
+                </ul></div>
