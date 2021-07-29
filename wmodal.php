73c9962163a9be364145c15a61946017c57090e8
M1lya <gilyazova2001@bk.ru>
2021-07-29 10:10:07 +0300

    Create wmodal.php

---
+++ wmodal.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text\html; charset=UTF-8" />
+  <title>Веб-интерфейс</title>
+  <link href="css/styles.css" rel="stylesheet" type="text/css" />
+</head>
+<body bgcolor="#e1f5fe">
+<form>
+<div>
+    <h1 align="center">Авторизация</h1>    
+    <p><label> Логин:<input name="login" type="text">
+    Пароль:<input name="password" type="password"></label></p> <br>
+  <div align="center"><button type="submit"><a href="#close">Войти</a> 
+  </button></div>
+  </div>
+</form>
+</body>
+</html>
